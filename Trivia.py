801cf2849ec11a004e7678cf232175eec4ac3ab4
Emilio Cabrera <emil12471@gmail.com>
2026-03-11 17:23:57 -0300

    Programa aCtualizado

--- Trivia.py
+++ Trivia.py
@@ -2,4 +2,34 @@
 # git add archivo.py
 # git status
 # git commit -m "mensaje"
-# git push
+# git push 
+#-----------------------------
+# trivia
+# es juego de triviaaa!
+# Challenge: Mini Trivia en Python
+
+## Debes crear:
+##Un archivo llamado `trivia.py`
+
+## Tu programa debe:
+##- pedir el nombre del jugador
+##- mostrar una bienvenida
+##- hacer 4 preguntas
+##- sumar 1 punto por cada respuesta correcta
+##- mostrar el nombre y el puntaje final
+
+## Resultado final
+##- si `puntaje == 4` → **Excelente**
+##- si `puntaje >= 2` → **Muy bien**
+##- si no → **Puedes mejorar**
+
+## Recuerda
+##- trabaja paso a paso
+##- no hace falta terminar perfecto
+##- usa Git durante el proceso
+##- haz varios commits pequeños
+
+nombre_del_jugador = input("dame tu nombre")
+print()
+
+
